6cb7965d7087d6a834d59500580a993b9a3b5c92
CJ Stanisci <cjstan22@gmail.com>
2020-02-07 21:39:19 -0700

    Aligned colors around pics with colors in map key

--- src/App.js
+++ src/App.js
@@ -57,8 +57,8 @@ function App() {
       }
       //bar color based on candidate    
       const color = {
-        'Joe Biden': 'rgb(55, 40, 150)',
-        'Bernie Sanders': 'rgb(60, 110, 205)',
+        'Joe Biden': 'rgb(41, 91, 255)',
+        'Bernie Sanders': 'rgb(67, 98, 165)',
         'Elizabeth Warren': 'rgb(125, 40, 230)',
         'Pete Buttigieg': 'rgb(85, 200, 255)',
         'Andrew Yang': 'rgb(0, 130, 120)'
